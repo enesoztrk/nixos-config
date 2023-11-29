cd1d50164db5682be152e8482bb86a1093689f04
Enes Öztürk <enes.ozturk@unikie.com>
2023-11-29 16:04:53 +0200

    updates

--- flake.nix
+++ flake.nix
@@ -1,18 +1,31 @@
-# flake.nix
+# SPDX-License-Identifier: MIT
 {
-  description = "My NixOS configuration";
+  description = "NixOS for thinkpad";
+
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-wayland.url = "github:nix-community/nixpkgs-wayland";
+    nixpkgs-wayland.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = { self, nixpkgs }: {
-    # System configuration
-    nixosConfigurations."mySystem" = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./hardware-configuration.nix
-        ./configuration.nix
+
+  outputs = inputs@{ self, nixpkgs,  ... } :
+  let
+    system = "aarch64-linux";
+    inherit (nixpkgs.lib) nixosSystem;
+    nixpkgsArgs = {
+      inherit system;
+      config.allowUnfree = true;
+    };
+  in {
+    nixosConfigurations."thinkpad" = nixpkgs.lib.nixosSystem {
+      inherit system;
+      specialArgs = { inherit system nixpkgs; };
+      modules = [({pkgs, ...}: {
+        nixpkgs.overlays = [ inputs.nixpkgs-wayland.overlay ];
+        })
+	    ./hardware-configuration.nix
+	    ./configuration.nix
       ];
     };
-    defaultPackage.x86_64-linux = self.nixosConfigurations."mySystem";
   };
 }
